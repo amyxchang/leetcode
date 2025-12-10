be999978693473417c73d67879c5850fa6e2f874
amyxchang <amychang.datasci@gmail.com>
2025-12-10 13:20:18 -0500

    Add function to identify big countries
    Implement function to filter big countries based on area and population.

--- 595_Big_Countries.py
+++ 595_Big_Countries.py
@@ -1,3 +1,12 @@
 """
 (blank python file for LeetCode problem 595 - Big Countries)
 """
+# completed 12/10/2025
+
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+
+    df = world[(world["area"] >= 3000000) | (world["population"] >= 25000000)]
+    df = df[["name","population","area"]]
+    return df
